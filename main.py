11624d14ef4702c2ab3d26f8f32a2e30c37742af
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-02-02 11:25:33 +0100

    some changes main

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ def chapters(who, number):
         for j in range(len(data[i]["books"])):
             if data[i]["books"][j]["name"] == who:
                 return data[i]["books"][j]["hadiths"][number]["info"]
-            
+            return "Book not found"
+        return "Hadith not found"
+    
 def hadiths(chapter, number):
     for i in range(len(data)):
         for j in range(len(data[i]["books"])):
             if data[i]["books"][j]["name"] == chapter:
                 return data[i]["books"][j]["hadiths"][number]["text"]
-            else:
-                return "Not Found"
+    return "Not Found"
+
 def by(chapter, number):
     for i in range(len(data)):
         for j in range(len(data[i]["books"])):
@@ -25,6 +27,7 @@ def by(chapter, number):
                 return "Not Found"
 
 def hadiths_beautyfier(hadith, by, chapter):
+    # Add a title to the hadith
     return "Hadith: " + hadith + "\n" + by + "\n" +"in" + chapter + "\n"
 
-print(hadiths_beautyfier(hadiths("1. Revelation", 2), by("1. Revelation", 2), chapters("1. Revelation", 2)))
+print(hadiths_beautyfier(hadiths("1. Revelation", 3), by("1. Revelation", 3), chapters("1. Revelation", 3)))
