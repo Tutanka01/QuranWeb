13bdcda0010bdf495ef01b0583ab37b8f3a6d482
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-02-02 09:47:31 +0100

    Modification du README et on pratique pour le main

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Bukhary_TEST
 J'essaie de faire un truc sympa avec le fichier json avec la sunnah écrite paralbukhary 
+# Nombre de livres :
+Il y a un totale de 9 volumes (livres), pour voir tous les titres : 
+<code>for i in range(len(data):
+    print(data[i]["name"]))
+</code>
--- main.py
+++ main.py
@@ -5,5 +5,14 @@ data = json.load(file)
 
 print(data[0]["books"][0]["hadiths"][0]["text"])
 
-for i in data[0]["books"][0]["hadiths"]:
-    print(i["info"])
+"""for i in data[0]["books"][0]["hadiths"]:
+    print(i["info"] + "\n",i["by"]+ "\n", i["text"])
+"""
+"""for i in data[0]["books"]: # 0 is the first book
+    print(i["name"])"""
+
+"""for i in data[7]["books"]: # 1 is the second book
+    print(i["name"])"""
+
+for i in range(len(data)):
+    print(data[i]["name"])
