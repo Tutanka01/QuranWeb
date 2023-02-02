1c97c5e2a00e2132b0356cd4171ed054112ac5a2
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-02-02 10:13:24 +0100

    quelques fonctionns pour le main

--- main.py
+++ main.py
@@ -8,12 +8,28 @@ def chapters():
         for j in range(len(data[i]["books"])):
             print(data[i]["books"][j]["name"])
             
-def hadiths(chapter, info):
+def hadiths(chapter, number):
     for i in range(len(data)):
         for j in range(len(data[i]["books"])):
             if data[i]["books"][j]["name"] == chapter:
-                return data[i]["books"][j]["hadiths"][info]["text"]
+                return data[i]["books"][j]["hadiths"][number]["text"]
             else:
                 return "Not Found"
+def by(chapter, number):
+    for i in range(len(data)):
+        for j in range(len(data[i]["books"])):
+            if data[i]["books"][j]["name"] == chapter:
+                return data[i]["books"][j]["hadiths"][number]["by"]
+            else:
+                return "Not Found"
+def chapter(chapter, number):
+    for i in range(len(data)):
+        for j in range(len(data[i]["books"])):
+            if data[i]["books"][j]["name"] == chapter:
+                return data[i]["books"][j]["hadiths"][number]["chapter"]
+            else:
+                return "Not Found"
+def hadiths_beautyfier(hadith, by, chapter, number):
+    return "Hadith: " + hadith + "\n" + "by" + by + "\n" +"in" + chapter + "\n" + "number" + number
 
-print(hadiths("The Book of Purification", 1))
+print(hadiths_beautyfier(hadiths("1. Revelation", 0), by("1. Revelation", 0), chapter("1. Revelation", 0), "0"))
