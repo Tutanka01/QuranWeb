9ec381b2d2d2ca1da0991cfb61474ddf65836777
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-02-16 09:58:49 +0000

    Ar_quran

--- Source/quran_formater.py
+++ Source/quran_formater.py
@@ -10,5 +10,12 @@ def quran_text_fr(nom): # Donne le nom du chapitre
                 print(data["sourates"][i]["versets"][k]["text"] + "\n")
             
             return "Sourate not found"
+def quran_text_ar(nom): # Donne le nom du chapitre
+    for i in range(0, len(data["sourates"])): # Il faut bien mettre "sourates"
+        if data["sourates"][i]["nom_phonetique"] == nom:
+            for k in range(len(data["sourates"][i]["versets"])):
+                print(data["sourates"][i]["versets"][k]["text_arabe"] + "\n")
+            
+            return "Sourate not found"
 
-print(quran_text_fr("Al-Baqara"))
+print(quran_text_ar("Al-Baqara"))
