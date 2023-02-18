af15b97b65fff8a64b71a1db2100f1f627ecd670
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-02-18 13:00:52 +0000

    Mise en marche du css et d'une protection contre les injections sql

--- Source/main_web.py
+++ Source/main_web.py
@@ -1,21 +1,22 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from quran_formater import quran_text_fr
-
-
 from flask import Flask, redirect, url_for, request
+from markupsafe import escape
+from quran_formater import *
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
    return render_template('login.html')
 
-@app.route('/login',methods = ['POST', 'GET']) # Here wher we put if it is a post or get request
+@app.route('/login',methods = ['POST', 'GET']) # Here wher we put if is a post or get request
 def login():
    if request.method == 'POST':
-      sourate = request.form['sourate']
-      return render_template('index.html', titre = sourate, nom_sourate = sourate, sourate_texte= quran_text_fr(sourate))
+      sourate = request.form['sourate'] 
+      return render_template('index.html', titre = escape(sourate), nom_sourate = escape(sourate), sourate_texte= quran_text_fr(escape(sourate))) # Il faut toujours mettre ka fonction esacpe pour eviter les attaques
    else:
       user = request.args.get('name')
       return render_template('login.html')
